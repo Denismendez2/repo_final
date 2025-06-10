5f9c6813509edbe00f127a4c0a8151353d5ad677
AndrewRD5530 <andrewportillo5530@gmail.com>
2025-06-09 18:27:37 -0600

    se creo el menu para toda la oagina usando vueity

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <template>
   <v-app>
-    
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Mi App Vue con Vuetify, Router y Pinia</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <router-link to="/" class="text-decoration-none text-white">
-        <v-btn text>Inicio</v-btn>
-      </router-link>
-      <router-link to="/about" class="text-decoration-none text-white">
-        <v-btn text>Acerca de</v-btn>
-      </router-link>
-    </v-app-bar>
-
+    <Menu />
     <v-main>
-      <router-view /> </v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
-
-
+import Menu from './components/MenuComponent.vue'
 </script>
-
-<style>
-/* Estilos para asegurar que los router-link se vean bien en la app-bar */
-.text-white {
-  color: white !important;
-}
-.text-decoration-none {
-  text-decoration: none !important;
-}
-</style>
